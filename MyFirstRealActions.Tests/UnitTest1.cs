227f01bebfdf89fc9f33b8f7ddf1eb8af5adbab0
LigiaKristiansen <ligiakristiansen@gmail.com>
2024-01-22 15:19:05 +0100

    Add testing workflow

---
+++ MyFirstRealActions.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using FluentAssertions;
+
+namespace MyFirstRealActions.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            string expected = "Hello World!";   
+            string result = "Hello World!";
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
